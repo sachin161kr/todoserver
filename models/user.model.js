28783bc5cdb7f99ef2d4a7cc54e2a484a845f779
Sachin Kumar <mail.sachin161@gmail.com>
2022-12-03 15:35:36 +0530

    database changes

--- models/user.model.js
+++ models/user.model.js
@@ -5,12 +5,6 @@ const User = new mongoose.Schema(
     name: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
-    todos: [
-      {
-        title: String,
-        desc: String,
-      },
-    ],
   },
   { collection: "userData" }
 );
